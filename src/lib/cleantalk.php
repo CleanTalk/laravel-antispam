7a426a449b287790d6e5636a705d66a3a745cf60
svfcode <svfcode@mail.ru>
2025-05-23 06:34:03 +0300

    Upd. Code. Init beta 3.0.0 version.

--- src/lib/cleantalk.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!config('cleantalk.enabled')) {
-	return;
-}
-// Set Cookies test for cookie test
-$apbct_timestamp = time();
-setcookie('apbct_timestamp',     $apbct_timestamp,                0, '/');
-setcookie('apbct_cookies_test',  md5(config('cleantalk.apikey').$apbct_timestamp), 0, '/');
--- src/views/widgets/cleantalk.blade.php
+++ src/views/widgets/cleantalk.blade.php
@@ -1,4 +1,1 @@
-<script>
-    const ctPublic = {"apbct_checkjs_val":"<?php echo md5(config('cleantalk.apikey')); ?>"}
-</script>
-<script src="{{ asset('vendor/cleantalk/ct_js_test.js')}}"></script>
+<script src="https://moderate.cleantalk.org/ct-bot-detector-wrapper.js"></script>
